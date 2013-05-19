26bf41526b1be04078fae26535b7274eca6e1da6
Fred Stakem <pi@pi.(none)>
2013-05-19 15:45:48 -0400

    Added gitignore

---
+++ pylapse.py
@@ -0,0 +1,40 @@
+# ------------------------------------------------------------------------------
+#
+#   pylapse.py
+#   By: Fred Stakem
+#   Date: 5.19.2013
+#
+# ------------------------------------------------------------------------------
+
+# Libraries
+import os
+
+# Setup
+script_name = os.path.basename(__file__)
+print '   ----------------< Starting %s >----------------   ' % (script_name)
+
+# Globals
+image_width = 1
+image_height = 1
+base_filename = 'xxx'
+output_path = ''
+number_of_images = 10
+time_between_images = 1
+
+# Functions
+# None
+
+# Data structures
+# None
+
+# Main
+print 'Starting main...'
+
+
+
+
+print 'Finished main.'
+
+print '   ----------------< Finishing %s >----------------   ' % (script_name)
+
+
